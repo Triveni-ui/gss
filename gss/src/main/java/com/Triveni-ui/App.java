603e5af27803a52b5eb8cb9056cdce6707471c5b
Triveni G <triveni28122002@gmail.com>
2025-05-15 21:06:07 +0530

    first commit

---
+++ gss/src/main/java/com/Triveni-ui/App.java
@@ -0,0 +1,13 @@
+package com.Triveni-ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
